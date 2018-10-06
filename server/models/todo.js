adc7107653a6a49825c30f886f529e9aef45fbbc
Robert D'Ippolito <robertdippolito@Roberts-MacBook-Pro-2.local>
2018-10-06 14:23:09 -0400

    refactored the code and added POST /todos route

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true, //value must exist
+    minLength: 1,
+    trim: true //removes beginning and trailing white space
+  },
+  complete: {
+    type: Boolean,
+    default: false //sets the default value
+  },
+  completedAt: {
+    type: Number,
+    default: null //should not exist if a user doesn't add a todo
+  }
+});
+
+module.exports = {
+  Todo: Todo
+}
