8473bb69d5792397e31cf3fb96feb9bdb75746be
Robert D'Ippolito <robertdippolito@Roberts-MacBook-Pro-2.local>
2018-10-08 10:44:08 -0400

    added new GET routes for todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
@@ -25,6 +26,21 @@ app.get('/todos', (req, res) => {
   },(e) => {
     res.status(400).send(e);
   });
+});
+
+app.get(`/todos/:id`, (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
